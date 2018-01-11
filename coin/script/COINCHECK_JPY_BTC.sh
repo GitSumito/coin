55d55f4c87443cd1c3261fa8c1e4fab7a0b5ece0
GitSumito <GitSumito@users.noreply.github.com>
2018-01-11 21:04:30 +0900

    Delete COINCHECK_JPY_BTC.sh

--- coin/script/COINCHECK_JPY_BTC.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-TABLE=COINCHECK_JPY_BTC
-PRICE=`curl -s https://coincheck.com/api/rate/btc_jpy | jq -r .rate | awk -F'.' '{print $1}'`
-
-echo "insert into $TABLE(price) values($PRICE);" > sql/$TABLE.sql;
